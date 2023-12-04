a6b90e444ac68a1cec5fe1259e8a899ea99ec112
Ahmad <ahmadfarouk799@gmail.com>
2023-12-04 09:36:11 +0100

    functionality to sign up page

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req , res, next ) => {
         await newUser.save()
         res.status(201).json("user created succesfully")
     }catch{
-       next(error);
+       next(error.message);
     }
    
 }
